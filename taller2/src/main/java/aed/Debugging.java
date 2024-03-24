7caa49c0b32e94590984c262dc6096cd2488b377
Carla Kim <carlakim138@gmail.com>
2024-03-24 14:15:30 -0300

    fixed functions and added comments explaining them, 3-64

--- taller2/src/main/java/aed/Debugging.java
+++ taller2/src/main/java/aed/Debugging.java
@@ -2,11 +2,19 @@ package aed;
 
 class Debugging {
     boolean xor(boolean a, boolean b) {
-        return a || b && !(a && b);
+        if(a != b) {
+            return true;
+         }
+        return false;
     }
+    //cambie la funcion completa por una condicional.
 
     boolean iguales(int[] xs, int[] ys) {
         boolean res = true;
+        
+        if(xs.length != ys.length) {
+            return false;
+        }
 
         for (int i = 0; i < xs.length; i++) {
             if (xs[i] != ys[i]) {
@@ -15,34 +23,41 @@ class Debugging {
         }
         return res;
     }
+    //agrege la condicion que ambas listas sean de igual tamaño.
 
     boolean ordenado(int[] xs) {
         boolean res = true;
-        for (int i = 0; i < xs.length; i++) {
-            if (xs[i] > xs [i+1]) {
+        for (int i = 0; i < xs.length - 1; i++) {
+            if (xs[i] > xs[i+1]) {
                 res = false;
             }
         }
         return res;
     }
+    //arregle la indexacion que se iba siempre a uno mas del tamaño de la lista.
 
     int maximo(int[] xs) {
-        int res = 0;
-        for (int i = 0; i <= xs.length; i++) {
-            if (xs[i] > res) res = i;
+        int res = xs[0];
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] > res) {
+                res = xs[i];
+            }
         }
         return res;
     }
+    //correji la asignacion de res por un elemento que sea parte de la lista, agregue los corchetes que faltaban del if y agrege al res el xs[i] que le faltaba.
 
     boolean todosPositivos(int[] xs) {
-        boolean res = false;
+        boolean res = true;
         for (int x : xs) {
             if (x > 0) {
                 res = true;
             } else {
-                res = false;
+                return false;
             }
         }
         return res;
     }
+    //correji que res sea true desde el inicio en el caso de la que lista sea vacia(?), arregle que una vez encuentren un negativo que retornen directamente false.
+
 }
